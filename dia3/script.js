e6c1778b628119f01e74c58b28ce88539ad7d532
vToria1025 <visctori1025@gmail.com>
2026-06-01 11:19:59 -0500

    Update script.js

--- dia3/script.js
+++ dia3/script.js
@@ -1,83 +1,47 @@
-/**
- * ARCHIVO: script.js
- * SISTEMA: Refugio Digital Día 03
- * DESCRIPCIÓN: Terminal interactiva con Kuromi ASCII, control estricto de etiquetas HTML e inyección directa de estilos.
- */
-
 const mensajes = {
-    pesado: "<div class='msg-header'>=========================================<br>  PROTOCOLO: DESCONEXIÓN Y CALMA v1.0<br>=========================================</div><br><br>Sé muy bien lo mucho que te esfuerzas día a día y la dedicación tan increíble que le pones a cada una de las tareas que manejas. <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>Aquí</span> tu manera de afrontar los retos en la oficina es admirable, pero a veces las rutinas se vuelven sumamente densas y el cansancio físico y mental empieza a pegar con mucha fuerza. Sé que el ritmo puede ser abrumador, pero justo hoy quería pasar por aquí para recordarte que <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>estoy</span> convencida de que está bien pausar, respirar profundo y soltar el control por un momento. No tienes que cargar con el peso del mundo sobre tus hombros; <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>siempre</span> mantén en mente que eres una mujer sumamente inteligente y tesa, y absolutamente nada de lo que ocurra en un día caótico va a cambiar lo valiosa que eres. Cuando por fin cierres la pantalla hoy, quiero que dejes el estrés ahí adentro. Recuerda que cuentas <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>contigo</span> misma para brillar, pero que también aquí estoy yo, celebrando cada uno de tus procesos, admirándote en silencio y completamente dispuesta a escucharte o a acompañarte en completo silencio, como tú lo prefieras. Desconéctate tranquila, descansa tu mente y recuerda que te mereces toda la paz del mundo, preciosa.",
+    pesado: "<div class='msg-header'>==== PROTOCOLO: CALMA ====</div><br>Sé bien lo mucho que te esfuerzas. <b>Aquí</b> tu manera de afrontar los retos es admirable, pero a veces el cansancio pega fuerte. <b>estoy</b> convencida de que está bien pausar. Recuerda que <b>siempre</b> eres valiosa. Cuentas <b>contigo</b> y conmigo, descansa preciosa.",
     
-    sonreir: "<div class='msg-header'>=========================================<br>  PROTOCOLO: SINCRONÍA Y COMPLICIDAD <br>=========================================</div><br><br>Si terminaste en este comando es porque seguramente al día le está haciendo falta un poquito de luz, y tienes que saber que mi misión favorita siempre va a ser recordarte lo bonito que es compartir la vida. <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>Tu</span> forma de ser es increíble, tienes una chispa única y una <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>felicidad</span> que contagia a cualquiera. Me pasa seguido que me quedo pensando en nuestras charlas, en las bobadas que solo nosotras entendemos, en los chistes internos y en cómo un día cualquiera <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>me</span> parece el mejor momento de la semana con solo ponernos a conversar un rato. Me <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>hace</span> inmensamente feliz saber que, aunque los kilómetros nos separen físicamente en el mapa, compartimos una sintonía tan linda y real. Hemos construido un espacio muy sano en donde nos sentimos <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>bien</span> y podemos hablar de todo con total transparencia, y eso es algo que valoro más de lo que te imaginas. Espero de todo corazón que leer esto te robe esa sonrisa gigante que tanto me gusta ver y que te recuerde que tu bienestar le da un color hermoso a mi realidad. ¡Te adoro un montón, Ame!",
+    sonreir: "<div class='msg-header'>==== PROTOCOLO: COMPLICIDAD ====</div><br><b>Tu</b> forma de ser es increíble y tienes una <b>felicidad</b> que contagia. Conversar contigo <b>me</b> parece el mejor momento de la semana. Me <b>hace</b> inmensamente feliz sentirnos <b>bien</b> hablando de todo. Te adoro, Ame!",
     
-    extrañar: "<div class='msg-header'>=========================================<br>  PROTOCOLO: ABRAZO A LA DISTANCIA <br>=========================================</div><br><br>Si te dio por presionar este protocolo, quiero pedirte que cierres los ojos un segundo y sientas este lazo tan fuerte que nos une. <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>Eres</span> una persona hermosa en todos los sentidos posibles, tanto por dentro como por fuera, de esas que esparcen <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>magia</span> por donde caminan por la forma tan bonita en que transformas todo a tu alrededor. La distancia puede parecer un obstáculo muy grande, pero se vuelve un número completamente tonto cuando alguien influye <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>en</span> tu día de la manera tan limpia, bonita y honesta en que lo hacemos nosotras. No pasa un solo día en el que no agradezca el haber coincido contigo en el camino; tu presencia le da un toque hermoso a <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>mi</span> realidad y me encanta escucharte, conocer cada día más de ti, compartir nuestros gustos y saber que estás al otro lado de la pantalla siendo un refugio seguro. No dudes jamás de lo mucho que te llevo conmigo en mis pensamientos. Este rinconcito que acompaña tu <span style='color:#ffffff!important;font-weight:bold;text-shadow:0 0 8px #c084fc;'>vida</span> lo creé con mis propias manos para que nunca tengas espacio a dudar de lo importante que eres para mí, preciosa. Estoy aquí para ti hoy, mañana y siempre."
+    extrañar: "<div class='msg-header'>==== PROTOCOLO: ABRAZO ====</div><br><b>Eres</b> una persona hermosa que esparce <b>magia</b> por donde camina. Influyes <b>en</b> mi día de forma única y tu presencia alegra <b>mi</b> realidad. Este refugio acompaña tu <b>vida</b>. Estoy aquí siempre."
 };
 
-const loginOutput = document.getElementById('login-output');
-const inputLine = document.getElementById('input-line');
-const accessKeyInput = document.getElementById('access-key');
-const loginAscii = document.getElementById('login-ascii');
-
-// Kuromi ASCII simétrica y corregida
 const kuromiArt = `
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣠⡶⠟⠷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡤⢤⣤⣤⡶⠟⠉⠉⠉⠋⢹⠛⠂⣄⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣤⣤⣴⠟⠁⠀⠀⠀⠀⠀⠀⢠⣄⠀⣼⠝⢄⡀⢈⡟⣿⡀⠀⠀⠀⠀⠀⢀⣼⣿⡀⣸⢳⣄⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⠶⠶⠶⠞⠋⠀⠀⠀⠀⢀⡄⠀⢠⡄⠀⠘⣦⡴⠞⠓⠒⠻⢦⣄⠀⠀⠐⢎⢿⣆⠀⠀⠀⠀⠸⣗⣮⣿⣯⣾⣿⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡶⠶⠟⠛⠁⠀⠀⠀⠀⠀⠀⠀⠠⣦⣠⡴⠿⠶⠾⢷⣄⡼⠋⢀⣤⠴⣄⠀⠀⠙⣇⠀⠀⠀⠊⣿⠀⠀⠀⠀⠀⢹⡆⢀⣨⠟⠁⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡏⠹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⠁⠀⠀⣠⡤⣤⡩⢿⡄⢸⡻⣿⣏⡇⠀⣴⡾⠛⢶⡀⠀⣿⠀⠀⠀⠀⠀⡼⢁⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠁⠀⠈⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⢸⣿⣿⣾⣿⠘⣷⠀⠙⠛⠋⢀⣼⠋⠀⠀⣸⡇⠀⠹⣧⡀⠀⠀⣼⠃⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢠⠄⢤⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠀⠀⠀⠑⠛⠓⠉⢰⡿⣤⣀⠀⢠⡾⠁⠀⠀⣰⠟⠀⠀⠀⠘⣷⠀⢰⠃⣸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠀⡇⠀⢸⠀⠈⠳⣄⡀⠀⠀⠀⠀⣀⡼⠷⠶⢦⡀⠀⠀⠀⣠⠟⠀⠀⠉⠽⠋⠀⣀⣴⣟⣉⣠⣄⠀⠀⠀⢹⢠⡏⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⢀⡴⢦⡄⠀⠀⢼⠁⠀⢇⣀⡜⠀⠀⠀⠈⠻⡆⠀⠀⡾⠁⠀⠀⠀⢀⡗⠒⠚⠋⠁⠀⠀⠀⠀⠠⠶⠛⠉⠀⣠⠴⠋⠀⠀⠀⠀⢸⣿⢀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⣴⠟⠓⣦⢿⣄⠀⠘⠻⣤⣀⠀⠀⠀⠀⢀⠀⠀⢿⡀⠀⠹⠦⠀⠀⠠⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡤⠖⠋⠹⣆⠀⠀⠀⠀⠀⠈⣿⢿⣅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⢠⡟⠉⢛⣧⡟⢿⡆⠀⠀⠈⠙⣷⡀⠀⠀⠀⠀⠀⠀⠛⢦⡀⠀⠀⠀⠀⠘⣝⠒⠒⠶⠶⠲⡖⠚⠋⠹⣇⢳⡀⢀⣠⠾⠃⠀⢠⠖⢠⣠⡿⠀⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠻⣯⣉⣀⣤⣌⠛⢶⣤⡀⠀⠘⢷⣄⠀⠀⠀⠀⢈⡇⠀⢷⠀⠀⠀⠀⠀⠘⢧⡀⠀⠀⠀⢻⠀⢀⣠⡼⠀⠓⠉⢱⠀⠀⠀⠸⣤⠜⢹⣇⣼⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠈⠁⠀⠈⠻⢦⣌⠙⠳⢦⣀⠉⠳⣄⠧⠠⠎⠀⠀⢸⡄⠀⠀⡠⢄⡀⠀⠻⣆⠀⣀⣤⣿⡽⠚⠉⢳⡀⠀⣸⠀⠀⠀⡠⠒⠦⡀⢻⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠦⣤⡈⠛⠶⢾⣋⠛⢦⡄⠀⠀⠙⣄⠀⢇⠀⠘⢆⠀⠈⠛⠧⣄⣀⣉⣀⣀⣼⣡⠴⠃⠀⠀⠀⡇⠀⢠⣇⣼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⣶⣤⣹⠆⠀⠙⣦⠀⠀⢸⠀⠈⠓⠒⠛⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠈⠒⠊⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣞⠉⠀⠀⠀⠘⡆⠀⠘⣧⠀⠀⠀⡔⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⣠⣤⣶⠿⠛⣾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠶⡶⠶⢿⣄⠀⠈⣇⠀⠀⠹⠞⠀⠀⠀⢀⣠⣤⡤⢴⡞⣻⠿⡉⣩⡽⠊⠁⣀⣰⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣇⣀⠀⠉⠉⠙⠻⢦⣤⡴⠞⠋⠛⠛⠋⠓⠲⣦⣎⠟⠻⡤⠾⠓⢋⣉⣩⣥⣼⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠉⠛⠲⠤⣄⣀⣸⠀⢀⣀⣀⡤⠤⠴⠒⢛⣉⣿⠀⠀⢿⣿⡆⢸⣿⠿⠿⠇⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠉⢹⠉⠉⠉⢰⣴⣶⣿⡇⢸⡿⠷⣄⣠⡞⠉⠁⠀⣀⣴⠶⠛⠙⣓⢲⠀⠀⠀⠀⠀⠀⢀⣀⣀⡀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣦⣄⡙⠳⣤⣀⠀⠀⠀⢸⠀⠀⠀⠘⠛⠉⠉⠁⠀⠀⢀⣨⣯⣤⡶⠶⢻⣏⠁⠀⢠⣞⡛⠺⣧⡀⠀⠀⠀⢰⡟⣿⡏⠉⡆⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣶⣾⣿⣿⣿⠀⣴⠟⠻⢶⣄⣸⠀⢀⣀⣀⣤⣴⡶⠶⠾⠛⠉⠉⠁⠀⠀⠀⠀⠈⠳⣄⡈⣁⣽⠷⢄⣩⠷⢤⣀⠀⠻⣿⣿⠀⠳⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡛⣿⢁⡿⠟⣠⠿⣦⡀⠀⢈⣽⠟⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠙⠢⣄⡈⠑⠦⣼⣿⣇⡇⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠴⠟⢠⣇⣼⣤⠞⢁⡴⠛⠛⠲⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⣤⣿⣿⣿⢻⡄⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠁⢉⣤⠖⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠿⣿⠜⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣽⣿⣻⣿⣻⣻⣿⡀⣋⣿⣿⠇
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣁⠀⠀⠀⠀⠀⣉⢉⠉⠀⠀⠀⢀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⢠⠋⠉⠉⠒⠲⢤⣀⣠⡀⠀
+⠀⠀⠀⠀⠀⠀⣀⣀⣀⢀⡠⠖⠋⠉⠀⠀⠀⠀⠉⠉⠢⣄⠀⠀⠀⢀⠼⠤⠇⠀
+⠀⠀⠀⣀⠔⠊⠁⠀⢨⠏⠀⠀⠀⣠⣶⣶⣦⠀⠀⠀⠀⠀⠱⣄⡴⠃⠀⠀⠀⠀
+⢸⣉⠿⣁⠀⠀⠀⢀⡇⠀⠀⠀⠀⢿⣽⣿⣼⡠⠤⢄⣀⠀⠀⢱⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠑⢦⡀⢸⠀⠀⠀⡠⠒⠒⠚⠛⠉⠀⢠⣀⡌⠳⡀⡌⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠉⠉⣆⠀⢰⠁⣀⣀⠀⠀⣀⠀⠈⡽⣧⢀⡷⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡤⢄⠀⠈⠢⣸⣄⢽⣞⡂⠀⠈⠁⣀⡜⠁⣩⡷⠿⠆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢯⣁⡸⠀⠀⠀⡬⣽⣿⡀⠙⣆⡸⠛⠠⢧⠀⡿⠯⠆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⡀⠀⠀⡤⠤⣵⠁⢸⣻⡤⠏⠀⠀⠀⠀⢹⠀⠀⠀⡊⠱⣀⠀⠀⠀
+⠀⠀⢀⠜⠀⢘⠀⠀⠱⠲⢜⣢⣤⣧⠀⠀⠀⠀⠀⢴⠇⠀⠀⠀⠧⠠⠜⠀⠀⠀
+⠀⠀⠘⠤⠤⠚⠀⠀⠀⠀⠀⠀⢸⠁⠁⠀⣀⠎⠀⠻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠣⣀⣀⡴⠤⠄⠴⠁⠀⠀
 `;
 
-const introText = "> Inyectando protocolos de seguridad...\n> Localizando servidor en México...\n> Acceso restringido.\n\n> PISTA: \"Yo las canciones y tú la...\"\n> Ingrese llave de acceso:";
-
-let isTyping = false;
+const introText = "> Inyectando protocolos...\n> Localizando servidor...\n> PISTA: \"Yo las canciones y tú la...\"\n> Acceso:";
 
 window.onload = () => {
-    loginAscii.textContent = kuromiArt;
-    typeEffect(introText, loginOutput, () => {
-        inputLine.style.display = 'block';
-        accessKeyInput.focus();
+    document.getElementById('login-ascii').textContent = kuromiArt;
+    typeEffect(introText, document.getElementById('login-output'), () => {
+        document.getElementById('input-line').style.display = 'block';
+        document.getElementById('access-key').focus();
     });
 };
 
-accessKeyInput.addEventListener('keypress', function (e) {
+document.getElementById('access-key').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        validarAcceso(this.value.toLowerCase().trim());
+        if (this.value.toLowerCase().trim() === 'magia') {
+            document.getElementById('login-screen').style.display = 'none';
+            document.getElementById('main-interface').style.display = 'block';
+        } else {
+            alert("Error de acceso.");
+            this.value = "";
+        }
     }
 });
-
-function validarAcceso(key) {
-    if (key === 'magia') { 
-        document.getElementById('login-screen').style.display = 'none';
-        document.getElementById('main-interface').style.display = 'block';
-    } else {
-        alert("Acceso denegado. Revisa la letra de la canción e intenta de nuevo.");
-        accessKeyInput.value = "";
-    }
-}
 
 function typeEffect(text, container, callback) {
     let i = 0;
@@ -89,42 +53,29 @@ function typeEffect(text, container, callback) {
             clearInterval(timer);
             if (callback) callback();
         }
-    }, 35);
+    }, 30);
 }
 
 function runCommand(tipo) {
-    if (isTyping) return;
-    
-    const mainOutput = document.getElementById('output');
     const typedDiv = document.getElementById('typed-output');
-    const ascii = document.getElementById('ascii-art');
     const text = mensajes[tipo];
-    
-    mainOutput.style.display = 'none';
-    ascii.style.display = 'none';
+    document.getElementById('output').style.display = 'none';
+    document.getElementById('ascii-art').style.display = 'none';
     typedDiv.innerHTML = "";
-    isTyping = true;
-
+    
     let i = 0;
     function typeWriter() {
         if (i < text.length) {
-            // Manejo de etiquetas HTML completo e instantáneo para evitar desarmar el texto o los estilos
             if (text.charAt(i) === '<') {
-                let closureTag = text.indexOf('>', i);
-                if (closureTag !== -1) {
-                    typedDiv.innerHTML += text.slice(i, closureTag + 1);
-                    i = closureTag + 1;
-                    typeWriter(); // Llama de inmediato para no meter retardos artificiales
-                    return;
-                }
+                let end = text.indexOf('>', i);
+                typedDiv.innerHTML += text.slice(i, end + 1);
+                i = end + 1;
+                typeWriter();
+                return;
             }
-            
             typedDiv.innerHTML += text.charAt(i);
             i++;
             setTimeout(typeWriter, 25);
-        } else {
-            isTyping = false;
-            typedDiv.innerHTML += "<br><br><p style='color:#666; font-size:12px;'>[ SISTEMA ] Ejecución terminada. Reinicia la página para volver al menú de opciones.</p>";
         }
     }
     typeWriter();
